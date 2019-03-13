ac90d923fdc9b28288787f8f3777acf4ff87cdf8
Zygimantas <36035840+zygios@users.noreply.github.com>
2019-03-13 17:37:59 +0200

    Update NRF24_sender_temperature.ino
    final version for testing

--- NRF24_sender_temperature.ino
+++ NRF24_sender_temperature.ino
@@ -20,31 +20,32 @@
 #define ONE_WIRE_BUS 6
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-#define NUM_SENSORS 1 
+#define NUM_SENSORS 6
 #define TEMPERATURE_PRECISION 9
 int temperature[NUM_SENSORS];
 DeviceAddress sensor_address[NUM_SENSORS] =
 {
+    { 0x28, 0x95, 0xF3, 0x44, 0x06, 0x00, 0x00, 0x93 },       // Lauko
+    { 0x28, 0xFF, 0x32, 0x34, 0x63, 0x16, 0x04, 0x6F },       // svetine
+    { 0x28, 0xFF, 0xFD, 0x73, 0x63, 0x16, 0x03, 0xC7 },       // zidinys
+    { 0x28, 0xFF, 0xB5, 0x36, 0x63, 0x16, 0x04, 0xF2 },       // garazas
+    { 0x28, 0xFF, 0x65, 0x0F, 0x62, 0x16, 0x04, 0xDC },       // hidroforas
     {0x28, 0xEB, 0x92, 0x44, 0x06, 0x00, 0x00, 0xC4}  //palepe
-  //{ 0x28, 0x95, 0xF3, 0x44, 0x06, 0x00, 0x00, 0x93 },       // Lauko
-  //{ 0x28, 0xFF, 0x32, 0x34, 0x63, 0x16, 0x04, 0x6F },       // svetine
-  //{ 0x28, 0xFF, 0xFD, 0x73, 0x63, 0x16, 0x03, 0xC7 },       // zidinys
-  //{ 0x28, 0xFF, 0xB5, 0x36, 0x63, 0x16, 0x04, 0xF2 },       // garazas
-  //{ 0x28, 0xFF, 0x65, 0x0F, 0x62, 0x16, 0x04, 0xDC },       // hidroforas
 };
 
 //nRF24L01
 RF24 radio(7,8);
-
-const uint64_t pipes = 0xABCDABCD71LL; // Needs to be the same for communicating between 2 NRF24L01 
+const uint64_t pipes = 0xB3B4B5B6F1LL; // Needs to be the same for communicating between 2 NRF24L01 
+const long RFUpdatePeriod =  60000L;  //60000=1min
+long RFLastUpdate = 0;
 
 typedef struct {
   int lauko; 
-  int palepe;
   int svetai; 
   int zidinys; 
-  int garazas
-  int hidroforas
+  int garazas;
+  int hidroforas;
+  int palepe;
 } dataPacket;
 
 dataPacket packet;
@@ -73,8 +74,24 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
   readTemp();
+  if(currentMillis - RFLastUpdate > RFUpdatePeriod) {
+    RFLastUpdate = currentMillis;
+    packet.lauko = temperature[0];
+    packet.svetai = temperature[1];
+    packet.zidinys = temperature[2];
+    packet.garazas = temperature[3];
+    packet.hidroforas = temperature[4];
+    packet.palepe = temperature[5];
+    
+    radio.stopListening();                                  // First, stop listening so we can talk.
+    if (radio.write(&packet, sizeof(dataPacket) )){
+      debugln(F("failed.")); 
+    }else{
+      debugln(F("Packed send!!."));
+    }
+  }
   delay(1000);
 }
 
@@ -88,7 +105,7 @@ void readTemp(){
       debugln(temperature[i]);
     }
     char TempBuffer[5];
-    sprintf(TempBuffer,"Temp_%d:%02d", 0, temperature[0]);
-    debugln(TempBuffer);
+    //sprintf(TempBuffer,"Temp_%d:%02d", i, temperature[i]);
+    //debugln(TempBuffer);
   }
 }
